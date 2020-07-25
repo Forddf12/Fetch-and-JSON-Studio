5ac12a80f2ede2387f330dbde121643f8c48369c
Doug F <forddf12@outlook.com>
2020-07-25 15:02:11 -0500

    finished version

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            //json accessible
+            let  container = document.getElementById("container");
+            for(let i = 0; i < json.length; i++) {
+                let skills = "";
+                for(let j = 0; j < json.length; j++) {
+                    skills += json[j].skills[j];
+                }
+                container.innerHTML += ` 
+                    <div class = "astronaut">
+                    <div class = "bio">
+                        <h3>${json[i].firstName + " " + json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                            <li>Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${json[i].picture}"></img>
+                    </div>
+                    `
+            }
+        })
+
+    })
+})
